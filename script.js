0866b3abbb7c3ffcf31c0acf8bfb875a738f9f77
Miguel Iglesias <mic15.mi@gmail.com>
2024-03-15 21:43:15 +0100

    Solucion temporal Promesa

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-let dataGlobal;
+'use strict';
 
 function initialize() {
-    
     getQuestions();
-    console.log(dataGlobal);
-    printQuestions(dataGlobal);
   
   
 }
 
-function getQuestions() {
-  const url =
-    "https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json";
-    return 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => dataGlobal = data);
-    
+//! Revisar/ cambiar por version nueva ECMA es6
+const getQuestions = async () => {
+  try {
+    const res = await fetch("https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json");
+    let data = await res.json();
+    printQuestions(data);
+  } catch (error) {
+      console.log(error);
+  }
 }
 
+
+//
 function printQuestions(questions) {
   const answerContainer = document.getElementsByClassName("answerContainer");
   
-  for (question of questions) {
-    
-    const questionH2 = document.getElementsByClassName("question")
-    questionH2.innerHTML = question.question
-    for (answer of question.answer) {
+  
+    const objectQuestions = questions[0];
+    const questionH2 = document.querySelector(".question")
+    questionH2.innerHTML = objectQuestions.question
+    const answersUl = document.querySelector(".answerContainer")
+    console.log(objectQuestions);
+    for (let answer of objectQuestions.answers) {
+        let stringLi = `<li>${answer}</li>`
         const answerlis = document.createElement("li");
-    }
-  }
+        answersUl.innerHTML += stringLi
+    } 
+    
 }
 
 document.addEventListener("DOMContentLoaded", initialize());
